4b4f6a1578b4afa10c8eb257cfdabe477f92082d
Ramiro-25 <rami-capp0@outlook.com>
2026-06-18 23:10:33 -0300

    Se agregaron capas

---
+++ Dsw2026Ej15.Data/PersistenceInMemory.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+using Dsw2026Ej15.Data.Dtos;
+using Dsw2026Ej15.Domain.Entities;
+using Dsw2026Ej15.Domain.Interfaces;
+
+namespace Dsw2026Ej15.Data;
+
+public class PersistenceInMemory : IPersistence
+{
+    private List<Speciality> _specialities = [];
+    private List<Doctor> _doctors = [];
+
+    public PersistenceInMemory()
+    {
+        LoadSpecialities();
+    }
+
+    private void LoadSpecialities()
+    {
+        try
+        {
+            string jsonPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Sources",
+                "specialities.json");
+
+            var json = File.ReadAllText(jsonPath);
+
+            var specialities = JsonSerializer.Deserialize<List<Dtos.SpecialityDto>>(
+                json,
+                new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true
+                }
+            ) ?? [];
+
+            _specialities = [
+                .. specialities.Select(s =>
+                    new Speciality(s.Name, s.Description, s.Id))
+            ];
+        }
+        catch (Exception)
+        {
+        }
+    }
+}
--- Dsw2026Ej15/Program.cs
+++ Dsw2026Ej15/Program.cs
@@ -1,4 +1,7 @@
 
+using Dsw2026Ej15.Data;
+using Dsw2026Ej15.Domain.Interfaces;
+
 namespace Dsw2026Ej15
 {
     public class Program
@@ -10,21 +13,19 @@ namespace Dsw2026Ej15
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<IPersistence, PersistenceInMemory>();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
 
             app.MapControllers();
 
